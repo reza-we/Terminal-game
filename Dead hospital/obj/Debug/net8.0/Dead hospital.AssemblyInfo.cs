b202b4b5ec8ad99c184bac4812927389a6b1e2b6
reza-we <s.reza.we@gmail.com>
2025-01-28 20:28:58 +0330

    fixing codes issue
    fixing solution issue with opening in  vs

--- Dead hospital/obj/Debug/net8.0/Dead hospital.AssemblyInfo.cs
+++ Dead hospital/obj/Debug/net8.0/Dead hospital.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Dead hospital")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+305c51acd9e8116859e632e8efa4ca20a2da7aa3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Dead hospital")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Dead hospital")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
